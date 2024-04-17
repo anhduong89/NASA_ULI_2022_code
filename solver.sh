832a4967d39da6d8adcc9e191843bb6ca825d4c3
anhduong89 <34546282+anhduong89@users.noreply.github.com>
2024-04-17 11:57:15 -0600

    test DL

--- solver.sh
+++ solver.sh
@@ -3,5 +3,5 @@
 DEFAULT_CONF=" cp_path.lp assign_charge.lp time_schedule.lp opt.lp init.lp base_data.lp --outf=0 -V0 -q1 --out-atomf=%s. | tr ' ' '\n'  "
 
 python gen_init.py $1
-clingo-dl cp_path.lp assign_charge.lp time_schedule.lp opt.lp init.lp base_data.lp --outf=0 -V0 -q1 -t64
+clingo-dl cp_path.lp assign_charge.lp time_schedule.lp opt.lp init.lp base_data.lp -n0
 # clingo-dl ${DEFAULT_CONF}
